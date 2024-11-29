a250782a189cc29b5a2eac572411c015f95281f3
eloabueno6 <00001091141228sp@al.educacao.sp.gov.br>
2024-11-28 23:11:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Portugues ',
+    'o q é verbo?',
+    'classe gramatical que expressa ação, estado ou fenômeno da natureza'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde esta localizado a Russica?',
+    'No continente Europeu'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quais as 3 perguntas que caracterizam a atitude filosófica?',
+    'Quem somos? de Onde Viemos? para onde vamos'
+)
+
+criaCartao(
+    'Artes',
+    'O que é cubismo ',
+    'movimento artístico que surgiu na França no início do século XX, rompendo com os padrões artísticos e culturais da época'
+)
